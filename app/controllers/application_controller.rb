bcc7ef50dd0a3b6d4a77365c2ec2e44250d55e0f
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-22 14:29:11 +0300

    Enable `name` param for registration & log-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exceptions
+
+    before_action :configure_permitted_parameters, if: :devise_contoller?
+
+    protected
+
+    def configute_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
