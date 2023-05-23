bdcdd2ce61c8a3965d7682c45710c18d6cbb838a
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-23 12:10:26 +0630

    Create public_recipes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :recipes
   resources :foods
   resources :users
+
+  get "/public_recipes", to: "recipes#public_recipes"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,72 @@ chicken_breast = Food.create(
   quantity: 10,
   user_id: user.id
 )
+
+simple_dish = Recipe.create(
+  name: 'Simple Dish',
+  preparation_time: '10 minutes',
+  cooking_time: '10 minutes',
+  description: 'A simple dish',
+  public: true,
+  user_id: 1
+)
+
+too_much = Recipe.create(
+  name: 'Too Much',
+  preparation_time: '10 minutes',
+  cooking_time: '10 minutes',
+  description: 'A complex dish',
+  public: true,
+  user_id: user.id
+)
+
+high_roller = Recipe.create(
+  name: 'High Roller',
+  preparation_time: '10 minutes',
+  cooking_time: '10 minutes',
+  description: 'A complex dish',
+  public: true,
+  user_id: user.id
+)
+
+recipe_food1 = RecipeFood.create(
+  quantity: 100,
+  recipe_id: simple_dish.id,
+  food_id: apple.id
+)
+
+recipe_food2 = RecipeFood.create(
+  quantity: 100,
+  recipe_id: simple_dish.id,
+  food_id: pineapple.id
+)
+
+recipe_food3 = RecipeFood.create(
+  quantity: 100,
+  recipe_id: simple_dish.id,
+  food_id: chicken_breast.id
+)
+
+recipe_food4 = RecipeFood.create(
+  quantity: 100,
+  recipe_id: too_much.id,
+  food_id: apple.id
+)
+
+recipe_food5 = RecipeFood.create(
+  quantity: 100,
+  recipe_id: too_much.id,
+  food_id: pineapple.id
+)
+
+recipe_food6 = RecipeFood.create(
+  quantity: 100,
+  recipe_id: high_roller.id,
+  food_id: apple.id
+)
+
+recipe_food7 = RecipeFood.create(
+  quantity: 100,
+  recipe_id: high_roller.id,
+  food_id: chicken_breast.id
+)
