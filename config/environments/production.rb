8bab91b61e3b286b82079b2f8c6762cc484019e6
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-26 11:48:59 +0300

    Add host to action-mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Rails_Group_Capstone_production"
 
+  config.action_mailer.default_url_options = { host: "https://ror-group-capstone.onrender.com/" }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
