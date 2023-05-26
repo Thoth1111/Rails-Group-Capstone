be058f61a459334fccff8108135e65deede1ed39
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-05-26 11:15:58 +0300

    edit action_mailer configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000}
+  config.action_mailer.default_url_options = { host: "https://ror-group-capstone.onrender.com/" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
